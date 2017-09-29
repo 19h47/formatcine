7b67b7d13bb9944596ecb29033a9ec86eb678280
Levron Jérémy <levronjeremy@19h47.fr>
2017-09-30 00:21:16 +0200

    Commit index.php and views folder

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * /index
+ *
+ * @package  	WordPress
+ * @subpackage  jveb
+ * @author   	Jérémy Levron levronjeremy@19h47.fr
+ */
+
+
+if ( ! class_exists( 'Timber' ) ) {
+	echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
+	return;
+}
+
+
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+
+
+$templates = array( 'index.twig' );
+
+
+Timber::render( $templates, $context );
