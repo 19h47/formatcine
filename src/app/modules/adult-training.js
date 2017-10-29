a44b19a97f3ecfd2e457fec0f86a3a1d0e2015d6
Levron Jérémy <levronjeremy@19h47.fr>
2017-10-29 20:03:16 +0100

    Update app

---
+++ src/app/modules/adult-training.js
@@ -0,0 +1,10 @@
+
+$('.js-adult-training').slick({
+	slidesToScroll: 1,
+	fade: true,
+	autoplay: true,
+	infinite: true,
+	arrows: false,
+	adaptiveHeight: true,
+	autoplaySpeed: 3000,
+});
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import Events from './app/modules/events';
 import SchoolTraining from './app/modules/school-training';
 import guid from './app/modules/guid';
 import Menu from './app/modules/Menu';
+import './app/modules/movie';
+import './app/modules/adult-training';
 
 window.app = new App();
 
@@ -14,52 +16,5 @@ guid();
 // Menu
 const MainMenu = new Menu();
 MainMenu.setupEvents();
-
-// Movies slider
-const sliders = document.querySelectorAll('.js-movie-slider');
-console.log(sliders);
-sliders.forEach((slider) => {
-	const $slider = $(slider);
-
-	$slider.find('.js-movie-slider-container').slick({
-		slidesToScroll: 1,
-		centerMode: true,
-		centerPadding: `${100 / 3}%`,
-		infinite: true,
-		arrows: true,
-		nextArrow: $slider.find('.js-movie-next'),
-		prevArrow: $slider.find('.js-movie-previous'),
-	});
-});
-
-const filters = document.querySelectorAll('.js-movie-filter');
-const containers = document.querySelectorAll('.js-movie-slider');
-
-filters.forEach((filter) => {
-	filter.addEventListener('click', function () {
-		if (this.classList.contains('is-active')) {
-			return;
-		}
-
-		const schoolClasses = JSON.parse(this.dataset.schoolClass);
-		let currentContainer = null;
-
-		filters.forEach((e) => {
-			e.classList.remove('is-active');
-		});
-
-		this.classList.add('is-active');
-
-		containers.forEach((e) => {
-			const categories = JSON.parse(e.dataset.schoolClass);
-			e.classList.remove('is-active');
-
-			schoolClasses.some((el) => {
-				currentContainer = e;
-				return categories.indexOf(el) >= 0 ? currentContainer.classList.add('is-active') : '';
-			});
-		});
-	});
-});
 
 console.log('Yo!');
